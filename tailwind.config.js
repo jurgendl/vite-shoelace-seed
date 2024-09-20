0670486ddf46a883885d37a42c090a328bfeba70
jurgen de landsheer <jurgen.delandsheer@ugent.be>
2024-09-20 09:25:33 +0200

    vite-shoelace-seed

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [
+		"./src/vite-shoelace-seed-index.html",
+		"./src/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
+
