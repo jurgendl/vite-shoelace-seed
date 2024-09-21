14c10f7f2a4126bec7baca37351829e96e259055
jurgen.de.landsheer <jurgen.de.landsheer@gmail.com>
2024-09-21 10:52:04 +0200

    vite-shoelace-seed

--- src/shoelace-theme-switch.js
+++ src/shoelace-theme-switch.js
@@ -1,9 +1,41 @@
 const darkModeThemeStorageKey = 'dark-mode';
 const lightModeName = 'sl-theme-light';
 const darkModeName = 'sl-theme-dark';
+const lightModeIcon = 'sun-fill';
+const darkModeIcon = 'moon-stars';
 const darkmodeToggleElementId = 'shoelace-theme-switch';
 
 const darkmodeToggleElement = document.getElementById(darkmodeToggleElementId);
+
+{
+	// Create a style element
+	const styleElement = document.createElement('style');
+	// Define the CSS rules
+	const cssRules = `
+		html {
+			color-scheme: light dark;
+		}
+		#shoelace-theme-switch {
+			cursor: pointer;
+			font-size: 24px;
+			position: absolute;
+			right: 10px;
+			top: 10px;
+			color: #ffea3d;
+			-webkit-filter: drop-shadow( 0 0 3px #B90000D4 ) drop-shadow( 0 0 5px #00000033 );
+			filter: drop-shadow( 0 0 3px #B90000D4 ) drop-shadow( 0 0 5px #00000033 );"
+		}
+		.sl-theme-dark #shoelace-theme-switch {
+			color: #eeeeee;
+			-webkit-filter: drop-shadow( 0 0 3px #FFE8E8 ) drop-shadow( 0 0 3px #000000ff );
+			filter: drop-shadow( 0 0 3px #FFE8E8 ) drop-shadow( 0 0 3px #000000ff );"
+		}
+	`;
+	// Set the CSS rules for the style element
+	styleElement.innerHTML = cssRules;
+	// Append the style element to the head of the document
+	document.head.appendChild(styleElement);
+}
 
 // Check if the dark mode preference is set to true
 function isDarkMode() {
@@ -16,11 +48,11 @@ function setDarkMode(isDarkMode) {
 	if (isDarkMode) {
 		document.documentElement.classList.remove(lightModeName);
 		document.documentElement.classList.add(darkModeName);
-		darkmodeToggleElement.name = 'moon-stars-fill';
+		darkmodeToggleElement.name = darkModeIcon;
 	} else {
 		document.documentElement.classList.remove(darkModeName);
 		document.documentElement.classList.add(lightModeName);
-		darkmodeToggleElement.name = 'sun-fill';
+		darkmodeToggleElement.name = lightModeIcon;
 	}
 	localStorage.setItem(darkModeThemeStorageKey, isDarkMode); // Store the dark mode preference in local storage
 	if (typeof changeDarkMode === 'function') changeDarkMode(isDarkMode); // Call the changeDarkMode function if it exists
@@ -39,8 +71,5 @@ document.addEventListener('DOMContentLoaded', () => setDarkMode(isDarkMode()));
 
 // Listen for changes in the user's preference
 if (window.matchMedia) {
-	window.matchMedia(`(prefers-color-scheme: ${darkModeName})`).addEventListener('change', event => {
-		const isDarkMode = event.matches;
-		setDarkMode(isDarkMode);
-	});
+	window.matchMedia(`(prefers-color-scheme: ${darkModeName})`).addEventListener('change', event => setDarkMode(event.matches));
 }
